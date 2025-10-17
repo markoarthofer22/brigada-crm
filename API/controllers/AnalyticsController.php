4aa22c61b2d690391148a80d565aeed4b0691783
Ivan Gudelj <ivan@borming.hr>
2025-10-17 10:35:40 +0200

    ...

--- API/controllers/AnalyticsController.php
+++ API/controllers/AnalyticsController.php
@@ -273,7 +273,6 @@ class AnalyticsController extends BaseController
 
 			// $item["zones"] = $zones;
 			$item["zones"] = $Analytics->groupZonesByIdZones($zones);
-			// $item["zones2"] = $Analytics->groupZonesByIdZones($zones);
 
 			foreach ($result_questions as $q) {
 				$r = array_values(array_filter(array_map(function ($a) use ($q) {
